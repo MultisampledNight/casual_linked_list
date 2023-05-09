cfb70b2a5679ee2ec8cec568ad04deb41b81b195
MultisampledNight <contact@multisamplednight.com>
2023-05-09 21:54:21 +0200

    Add test for pop_*

--- src/tests.rs
+++ src/tests.rs
@@ -22,3 +22,33 @@ fn casual_push_and_observe() {
         vec![&"qwq", &"xwx", &"owo", &"uwu", &"kwk", &"-w-"]
     );
 }
+
+#[test]
+fn snake_and_reverse() {
+    let mut snake = ReversibleList::new();
+
+    snake.push_back(10);
+    snake.push_front(-45);
+    snake.push_front(-7);
+    snake.push_back(1_000_000);
+    snake.push_back(10);
+    snake.push_front(-30);
+
+    // nom
+    assert_eq!(snake.pop_back(), Some(10));
+    assert_eq!(snake.pop_front(), Some(-30));
+    assert_eq!(snake.pop_front(), Some(-7));
+
+    snake.push_front(1);
+
+    assert_eq!(snake.iter().sum::<i32>(), snake.iter().rev().sum());
+    assert_eq!(snake.iter().copied().collect::<Vec<_>>(), vec![1, -45, 10, 1_000_000]);
+
+    // trying to pop an already empty list should not panic
+    for _ in 0..10 {
+        snake.pop_front();
+        snake.pop_back();
+    }
+    assert_eq!(snake.pop_front(), None);
+    assert_eq!(snake.pop_back(), None);
+}
